11f301354a42202657972ab6ca7cbf1965215577
Elie <79168733+el2zay@users.noreply.github.com>
2022-04-09 18:00:59 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+// ignore_for_file: unnecessary_brace_in_string_interps, unnecessary_new
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+//import keyboard
+void main() {
+  runApp(const TapboxA());
+}
+
+var randomColor = Color.fromRGBO(
+    Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), 1.0);
+var r = randomColor.red;
+var g = randomColor.green;
+var b = randomColor.blue;
+
+
+class TapboxA extends StatefulWidget {
+  const TapboxA({Key? key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            Color.fromRGBO(
+                r = Random().nextInt(256), // Red color
+                g = Random().nextInt(256), // Green color
+                b = Random().nextInt(256), // Blue color
+                1); // Opacity
+          });
+        },
+        child: MaterialApp(
+            home: Scaffold(
+                backgroundColor: Color.fromRGBO(r, g, b, 1),
+                body: Center(
+                    child: Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: Column(
+                          //Centrer le texte
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Center(
+                                child: Padding(
+                              padding: const EdgeInsets.all(30),
+                              child: InkWell(
+                                //Mettre un padding pour que le texte soit plus visible
+
+                                child: Text(
+                                  'RGB : ${r} ${g} ${b}',
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontFamily: 'RobotoLight',
+                                    color: _color(),
+                                  ),
+                                ),
+
+                                onTap: () {
+                                  Clipboard.setData(new ClipboardData(
+                                      text: '${r} ${g} ${b}'));
+                                },
+                                onDoubleTap: () {
+                                  launch(
+                                      'https://htmlcolorcodes.com/fr/rgb-a-hex/?r=$r&g=$g&b=$b');
+                                },
+                                onLongPress: () {
+                                  launch(
+                                      'https://htmlcolorcodes.com/fr/rgb-a-hex/?r=$r&g=$g&b=$b');
+                                },
+                              ),
+                            )),
+                            new InkWell(
+                              child: Text(
+                                  'HEX : ${r.toRadixString(16) + g.toRadixString(16) + b.toRadixString(16)}'
+                                      .replaceAll('ff', '#'),
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontFamily: 'RobotoLight',
+                                    color: _color(),
+                                  ),
+                                  textAlign: TextAlign.center),
+                              onTap: () {
+                                Clipboard.setData(new ClipboardData(
+                                    text: r.toRadixString(16) +
+                                        g.toRadixString(16) +
+                                        b
+                                            .toRadixString(16)
+                                            .replaceAll('ff', '#')));
+                              },
+                              onDoubleTap: () {
+                                launch(
+                                    'https://htmlcolorcodes.com/fr/hex-a-rgb/?hex=${r.toRadixString(16) + g.toRadixString(16) + b.toRadixString(16)}'
+                                        .replaceAll('ff', ''));
+                              },
+                              onLongPress: () {
+                                launch(
+                                    'https://htmlcolorcodes.com/fr/hex-a-rgb/?hex=${r.toRadixString(16) + g.toRadixString(16) + b.toRadixString(16)}'
+                                        .replaceAll('ff', ''));
+                              },
+                            ),
+                          ],
+                        ))))));
+  }
+}
+
+_color() {
+  if (r + g + b >= 384) {
+    return Colors.black;
+  } else {
+    return Colors.white;
+  }
+}
